628f299970f5f0afc76c96b7212ae5209b106a11
xappyyy <surin123.james@gmail.com>
2025-09-16 22:41:55 +0700

    add ci

--- server/eslint.config.cjs
+++ server/eslint.config.cjs
@@ -1,34 +1,26 @@
-const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
-const prettierPlugin = require("eslint-plugin-prettier");
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = [
   {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
 
-    ignores: [
-      "node_modules/**",
-      "dist/**",
-      "build/**",
-      "pkg/grpc/generated/**",
-    ],
+    ignores: ['node_modules/**', 'dist/**', 'build/**', 'pkg/grpc/generated/**'],
 
     languageOptions: {
-      parser: require("@typescript-eslint/parser"),
+      parser: require('@typescript-eslint/parser'),
       parserOptions: {
         ecmaVersion: 2020,
-        sourceType: "module",
-        project: "./tsconfig.json",
+        sourceType: 'module',
+        project: './tsconfig.json',
       },
     },
 
     plugins: {
-      "@typescript-eslint": typescriptPlugin,
-      prettier: prettierPlugin,
+      '@typescript-eslint': typescriptPlugin,
     },
 
     rules: {
       ...typescriptPlugin.configs.recommended.rules,
-      "prettier/prettier": "error",
     },
   },
 ];
